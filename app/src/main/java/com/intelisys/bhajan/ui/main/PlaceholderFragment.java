d9e5fd3051da94bae947852a0d5e6af4b5192cc2
Priyam <priyam.ds@somaiya.edu>
2020-06-28 16:26:14 +0530

    updates ui

--- app/src/main/java/com/intelisys/bhajan/ui/main/PlaceholderFragment.java
+++ app/src/main/java/com/intelisys/bhajan/ui/main/PlaceholderFragment.java
@@ -43,19 +43,19 @@ public class PlaceholderFragment extends Fragment {
         pageViewModel.setIndex(index);
     }
 
-    @Override
-    public View onCreateView(
-            @NonNull LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_switch_tab, container, false);
-        final TextView textView = root.findViewById(R.id.section_label);
-        pageViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        //Button btn =
-        return root;
-    }
+//    @Override
+//    public View onCreateView(
+//            @NonNull LayoutInflater inflater, ViewGroup container,
+//            Bundle savedInstanceState) {
+//        View root = inflater.inflate(R.layout.fragment_switch_tab, container, false);
+//        final TextView textView = root.findViewById(R.id.section_label);
+//        pageViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+//        //Button btn =
+//        return root;
+//    }
 }
